b5b94c0169e1d1c15472b51c2941085940244f97
Mohammad Sabbagh <mohammad.sabbagh@gmail.com>
2022-03-01 22:33:54 +0300

    yellow

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,22 +3,22 @@ import React from "react"
 export default function Contacts() {
 
   return (
-    <section className="section is-medium contacts has-text-black has-background-primary" id="contacts">
+    <section className="section is-medium contacts has-text-black" id="contacts">
       <div className="container">
         <h3 className="section-title">Contacts</h3>
 
         <div className="columns">
 
           <div className="column has-text-centered">
-            <p>DEV Earthing & Lighting Protection Services CC</p>
+            <p><b>DEV Earthing & Lighting Protection Services CC</b></p>
             <p>P.O. Box 859</p>
             <p>Swakopmund, Namibia</p>
             <p><a href="tel:+264816950045">+264 81 695 0045</a></p>
           </div>
 
           <div className="column has-text-centered">
-            <h4>Managing Director:</h4>
-            <p>Chris de Villiers</p>
+            <h4><b>Chris de Villiers</b></h4>
+            <p>Managing Director</p>
             <p><a href="mailto:cdevilliersearthing@gmail.com">cdevilliersearthing@gmail.com</a></p>
           </div>
 
